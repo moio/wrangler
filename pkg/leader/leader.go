c1d541c08bc2824add83a11c442f1fc844c5176f
Silvio Moioli <silvio@moioli.net>
2023-05-31 11:45:05 +0200

    leaderelection: configure all timeouts via environment variables
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- pkg/leader/leader.go
+++ pkg/leader/leader.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/leaderelection"
@@ -12,6 +13,13 @@ import (
 )
 
 type Callback func(cb context.Context)
+
+const defaultLeaseDuration = 45 * time.Second
+const defaultRenewDeadline = 30 * time.Second
+const defaultRetryPeriod = 2 * time.Second
+
+const developmentLeaseDuration = 45 * time.Hour
+const developmentRenewDeadline = 30 * time.Hour
 
 func RunOrDie(ctx context.Context, namespace, name string, client kubernetes.Interface, cb Callback) {
 	if namespace == "" {
@@ -43,16 +51,37 @@ func run(ctx context.Context, namespace, name string, client kubernetes.Interfac
 		logrus.Fatalf("error creating leader lock for %s: %v", name, err)
 	}
 
-	t := time.Second
-	if dl := os.Getenv("CATTLE_DEV_MODE"); dl != "" {
-		t = time.Hour
+	leaseDuration := defaultLeaseDuration
+	renewDeadline := defaultRenewDeadline
+	retryPeriod := defaultRetryPeriod
+	if d := os.Getenv("CATTLE_DEV_MODE"); d != "" {
+		leaseDuration = developmentLeaseDuration
+		renewDeadline = developmentRenewDeadline
+	}
+	if d := os.Getenv("CATTLE_ELECTION_LEASE_DURATION"); d != "" {
+		leaseDuration, err = time.ParseDuration(d)
+		if err != nil {
+			return errors.Wrapf(err, "CATTLE_ELECTION_LEASE_DURATION is not a valid duration")
+		}
+	}
+	if d := os.Getenv("CATTLE_ELECTION_RENEW_DEADLINE"); d != "" {
+		renewDeadline, err = time.ParseDuration(d)
+		if err != nil {
+			return errors.Wrapf(err, "CATTLE_ELECTION_RENEW_DEADLINE is not a valid duration")
+		}
+	}
+	if d := os.Getenv("CATTLE_ELECTION_RETRY_PERIOD"); d != "" {
+		retryPeriod, err = time.ParseDuration(d)
+		if err != nil {
+			return errors.Wrapf(err, "CATTLE_ELECTION_RETRY_PERIOD is not a valid duration")
+		}
 	}
 
 	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
 		Lock:          rl,
-		LeaseDuration: 45 * t,
-		RenewDeadline: 30 * t,
-		RetryPeriod:   2 * t,
+		LeaseDuration: leaseDuration,
+		RenewDeadline: renewDeadline,
+		RetryPeriod:   retryPeriod,
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: func(ctx context.Context) {
 				go cb(ctx)
